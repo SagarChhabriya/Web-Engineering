aaca6234dba0e3ea5820e4d7836af94c20d370ac
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-09-03 10:34:13 +0500

    Add Web 5-Class_Sept_02

---
+++ Sem-5-Web/5-Class_Sept_03_24_InnerHTML/app.js
@@ -0,0 +1,7 @@
+function asc() {
+    // to change the text
+    document.getElementById("q").innerHTML = "IBA KK";
+
+    // to change the image
+    document.getElementById("j").src = "work-2.png";
+}
