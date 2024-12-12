581cb2758eeb65cb1ffb21e035b1e052e5a27481
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-12-12 10:38:36 +0500

    Add Lab Final Task

---
+++ 15-Web-Final-Lab/app.js
@@ -0,0 +1,14 @@
+function loadFile() {
+
+    let request = new XMLHttpRequest();
+    request.open("GET", "sample.txt", true);
+
+    request.onreadystatechange = function () {
+        document.querySelector(".data").innerHTML = this.responseText;
+    }
+
+    request.send();
+
+
+
+}
