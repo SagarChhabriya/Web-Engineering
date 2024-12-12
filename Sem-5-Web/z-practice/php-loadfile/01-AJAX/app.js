08260d62c92482293e9cd821c69c488f5387a7e6
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-12-12 05:04:55 +0500

    Update

---
+++ Sem-5-Web/z-practice/php-loadfile/01-AJAX/app.js
@@ -0,0 +1,13 @@
+function loadFile() {
+
+    let request = new XMLHttpRequest();
+    request.open("GET", "sample.txt", true);
+    request.onreadystatechange = function () {
+
+        document.querySelector(".data").innerHTML = this.responseText;
+    };
+
+    request.send();
+
+
+}
