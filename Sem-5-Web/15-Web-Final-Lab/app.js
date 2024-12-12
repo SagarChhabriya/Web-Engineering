c6ce5e81e00c0dc0ca3e71072c4aaa520e5b5f45
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-12-12 10:42:58 +0500

    Upadte Final Task Path

--- Sem-5-Web/15-Web-Final-Lab/app.js
+++ Sem-5-Web/15-Web-Final-Lab/app.js
@@ -1,14 +1,8 @@
 function loadFile() {
-
-    let request = new XMLHttpRequest();
+    let request = new XMLHttpRequest(); 
     request.open("GET", "sample.txt", true);
-
     request.onreadystatechange = function () {
         document.querySelector(".data").innerHTML = this.responseText;
     }
-
     request.send();
-
-
-
 }
