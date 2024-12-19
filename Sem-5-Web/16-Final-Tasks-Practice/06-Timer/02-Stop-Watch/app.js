7064e92dbac5c33e4652410c724936486c76c15b
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2024-12-19 19:59:35 +0500

    Add StopWatch Code

---
+++ Sem-5-Web/16-Final-Tasks-Practice/06-Timer/02-Stop-Watch/app.js
@@ -0,0 +1,27 @@
+let hour = 0;
+let mnt = 0;
+let sec = 0;
+function asc() {
+
+
+    sec++;
+
+    if (sec > 59) {
+        sec = 0;
+        mnt = mnt + 1;
+    }
+    if (mnt > 59) {
+        mnt = 0;
+        hour = hour + 1;
+    }
+    if (hour > 24) {
+        sec = 0;
+        mnt = 0;
+        hour = 0;
+    }
+
+    document.getElementById("sec").innerHTML = sec;
+    document.getElementById("mnt").innerHTML = mnt + " : ";
+    document.getElementById("hour").innerHTML = hour + " : ";
+}
+setInterval(asc, 0.1);
